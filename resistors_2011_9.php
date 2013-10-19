8a39b201389209dab77826426e72d0de14e345de
Filippos <phil.230@gmail.com>
2013-10-19 22:58:33 +0300

    first implementation, complicated data structures

--- resistors_2011_9.php
+++ resistors_2011_9.php
@@ -2,69 +2,23 @@
 
 //Classes
 
-class ball {
-	public $position;
-	public $speed;
+class resistor {
+	public $value;
+	public $start;
+	public $end;
 
-	public function __construct($pos,$speed) {
-		$this->position = $pos;
-		$this->speed = $speed;
+	public function __construct($start,$end) {
+		$this->value = 1;
+		$this->start = $start;
+		$this->end = $end;
 	}
-
-	public function collision() {
-		$this->speed = -1 * $this->speed;
-	}
-
-	public function move($time) {
-		$pos = $this->position;
-		$pos += $this->speed * $time;
-		if ($pos < 0) {
-			return 0;
-		} elseif ($pos > 100) {
-			return 100;
-		} else { 
-			return $pos; 
-		}
-	}
-
 }
 
 //Functions 
 
-function if_collision($ball1, $ball2, $t0){
-
-	$x1 = $ball1->position;
-	$v1 = $ball1->speed;
-	$x2 = $ball2->position;
-	$v2 = $ball2->speed;
-
-	//Same Speed - No collision
-	if ( $v1 == $v2){
-		return -1;
-	}
-	//Same Direction
-	if ( $v1 * $v2 > 0 ) {
-		if ( $v1 > $v2){
-			return (($x2 - $x1)/($v1-$v2) + $t0) ; 		
-		}
-		else {
-			return -1;
-		}
-	}
-	//Opposite Direction
-	else {
-		if ($v1 > 0) {
-			return (($x2 - $x1)/($v1-$v2) + $t0) ; 		
-		}
-		else{
-			return -1;		
-		}	
-	}	
-}
-
+// GET INPUT
 $input = array();
 
-// GET INPUT
 fscanf(STDIN, "%d\n", $N);
 for ($i = 0; $i<$N; $i++) { 
 	$string = fgets(STDIN);
@@ -72,75 +26,38 @@ for ($i = 0; $i<$N; $i++) {
 }
 //------------------------------------------------
 
-
-$balls = array();
+$resistors = array();
+$nodes = array();
+$ends = array();
 foreach ($input as $data) {
 
 	//Interpret Input
-	$ballnum = $data[0];
-	for ($i = 0;$i < $ballnum; $i++) {
-		$k = $i*2;
-		$balls[$i] = new ball($data[$k+1],$data[$k+2]);
-	}
-	$length = count($data);
-	$finball = $data[$length-2] - 1;
-	$fintime = $data[$length-1];	
+	//$resistors = array of resistor objects
+	//$nodes = array of node arrays (x resistors for each node)
+	//$ends = specific array for az nodes
+	foreach ($data as $letter) {
+		$resistor = new resistor($letter[0], $letter[1]);
 
-
-	// 1 Ball
-	if ($ballnum == 1) {
-		echo $balls[$finball]->move($fintime),"\n";
-	}	
-	// 2 Balls	
-	elseif ( $ballnum == 2) { 
-		$t = if_collision($balls[0], $balls[1], 0);	
-
-		//If no collision, move finball normally 
-		if ( ($t == -1) || ($t > $fintime) ){
-			echo $balls[$finball]->move($fintime),"\n";
+		if ($letter[0] == 'a') {
+			$ends['a'][] 		= $resistor;
+		} else {
+			$nodes[$letter[0]][] 	= $resistor;
 		}
-		//If collision: Move, Collide, Move
-		else{
-			$balls[$finball]->position = $balls[$finball]->move($t);
-			$balls[$finball]->collision();
-			echo $balls[$finball]->move($fintime-$t),"\n";
+
+		if ($letter[1] == 'z') {
+			$ends['z'][] 		= $resistor;
+		} else {
+			$nodes[$letter[1]][] 	= $resistor;
+		}
+		$resistors[] = $resistor;
+	}
+
+	/*foreach ($nodes as $node) {
+		if ( count($node) == 2 ) {
+			
 		}
 	}	
-
-	//3 Balls
-	elseif ( $ballnum > 2) {
-		$tmin = 0;
-		$t0 = 0;
-		while ($tmin <= $fintime) {
-
-			$tmin = 99999;
-			$coll = -1;
-			for ($i	=0; $i< $ballnum-1; $i++) {
-				//Check if pair of balls collides
-				$t = if_collision($balls[$i], $balls[$i+1], $t0);
-				
-				//Find first collision
-				if ( ($t < $tmin) && ($t != -1) ) { 
-					$tmin = $t;
-					$coll = $i;
-				}
-			}
-			if ($tmin <= $fintime) {
-				
-				//Move all balls 
-				for ($k=0; $k < $ballnum; $k++) {
-					$balls[$k]->position = $balls[$k]->move($tmin-$t0);
-				}
-		
-				//Collide only the two
-				$balls[$coll]->collision();
-				$balls[$coll+1]->collision();
-
-				$t0 = $tmin;
-			}
-		}
-		echo $balls[$finball]->move($fintime-$t0),"\n";
-	}
+	*/
 }	
 
 
