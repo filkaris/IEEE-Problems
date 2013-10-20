2a1b3b8f0a282a0d2ccf0419a48053ecf49ef7fe
Filippos <phil.230@gmail.com>
2013-10-20 15:14:30 +0300

    in the middle of new structs

--- resistors_2011_9.php
+++ resistors_2011_9.php
@@ -2,19 +2,58 @@
 
 //Classes
 
-class resistor {
+class Resistor {
+	public $name;
 	public $value;
 	public $start;
 	public $end;
 
-	public function __construct($start,$end) {
-		$this->value = 1;
-		$this->start = $start;
-		$this->end = $end;
+	public function __construct($letters,$value = 1) {
+		$this->value = $value;
+		$this->start = $letters[0];
+		$this->end = $letters[1];
+		$this->name = $letters;
+	}
+
+	public function in_series($resistor,$nodes) {
+		$this->value += $resistor->value;
+
+		if ($this->start == $resistor->end) {
+			$this->start = $resistor->start;
+		}
+		else {
+			$this->end = $resistor->end;			
+		}
 	}
 }
+/*
+class Node {
+	public $name;
+	public $resistors;
 
+	public function __construct($name) {
+		$this->name = $name;
+		$this->resistors = array();
+	}
+}
+*/
 //Functions 
+
+function in_series($res1, $res2) {
+	$value = $res1->value + $res2->value;
+	
+	if ($res1->start == $res2->end) {
+		$nstart = $res2->start;
+		$nend = $res1->end;
+	}
+	else {	
+		$nstart = $res1->start;
+		$nend = $res2->end;
+	}	
+
+	return new Resistor($nstart.$nend,$value);
+	
+}
 
 // GET INPUT
 $input = array();
@@ -26,39 +65,72 @@ for ($i = 0; $i<$N; $i++) {
 }
 //------------------------------------------------
 
-$resistors = array();
+$resistor = array();
+$connections = array();
 $nodes = array();
+foreach(range('a','z') as $i) {
+	$nodes[$i] = 0;
+}
 $ends = array();
-foreach ($input as $data) {
-
+//foreach ($input as $data) {
+$data = $input[0];
 	//Interpret Input
 	//$resistors = array of resistor objects
 	//$nodes = array of node arrays (x resistors for each node)
 	//$ends = specific array for az nodes
+
 	foreach ($data as $letter) {
-		$resistor = new resistor($letter[0], $letter[1]);
-
-		if ($letter[0] == 'a') {
-			$ends['a'][] 		= $resistor;
-		} else {
-			$nodes[$letter[0]][] 	= $resistor;
-		}
-
-		if ($letter[1] == 'z') {
-			$ends['z'][] 		= $resistor;
-		} else {
-			$nodes[$letter[1]][] 	= $resistor;
-		}
-		$resistors[] = $resistor;
+		$resistor[$letter][] = new Resistor($letter);
+		$connections[] = $letter;
+		$nodes[$letter[0]]++;
+		$nodes[$letter[1]]++;
 	}
+	//var_dump($resistor);
+	//var_dump($connections);
+	//var_dump($ends);
 
-	/*foreach ($nodes as $node) {
-		if ( count($node) == 2 ) {
+	$nodes = array_filter($nodes);
+	foreach ($nodes as $key => $resno) {
+		var_dump($nodes);
+		if ($key == 'a' || $key == 'z')
+			continue;
+		if ($resno == 2) {
+			//Return 'bc' , 'cd' for key = c
+			echo $key."\n";
+			$conns = preg_grep('/['.$key.']/',$connections);
+
+			//Calculate in series
+			$new = in_series($resistor[$conns[0]][0],$resistor[$conns[1]][0]);
+
+			//Deleting OLDS
+			foreach ($conns as $keys => $letters) {
+				//Remove them from connections			
+				unset($connections[$keys]);
+
+				//Remove them from resistors
+				//There's only one for sure, else we could not do in series
+				unset($resistor[$letters]);
+
+				//Remove them from nodes
+				$nodes[$letters[0]]--;
+				$nodes[$letters[1]]--;
+			}	
+			$nodes = array_filter($nodes);	
+
+			//Adding NEW
+			$name = $new->name;
+			$connections[] = $name;
+			$resistor[$name][] = $new;
+			$nodes[$name[0]]++;
+			$nodes[$name[1]]++;
 			
 		}
-	}	
-	*/
-}	
+	}
+	echo "------------------------------------------------------------------\n";
+	//var_dump($resistor);
+	//var_dump($nodes);
+	//var_dump($ends);
+//}	
 
 
 ?>
